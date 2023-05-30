38781cc8087b06eb0256dbea2b549445b10c3503
SiyamthandaMarimani <134868791+SiyamthandaMarimani@users.noreply.github.com>
2023-05-30 22:20:42 +0200

    Update script.js
    save to s3

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const fileInput = document.getElementById('fileInput');
-    const previewImage = document.getElementById('previewImage');
-  
-    fileInput.addEventListener('change', (event) => {
-      const file = event.target.files[0];
-      const reader = new FileReader();
-  
-      reader.onload = function (e) {
-        previewImage.src = e.target.result;
-      };
-  
-      reader.readAsDataURL(file);
-    });
+const AWS = require('aws-sdk');
+
+// Configure AWS SDK with your credentials and region
+AWS.config.update({
+  accessKeyId: 'AKIAZQ6OBW4AC37XRYK4',
+  secretAccessKey: 'LKncx651Tx/OpRFb45GfILRy9BrL+nH+hnsRupSs',
+  region: 'us-west-2' // Replace with your desired AWS region
+});
+
+// Create an S3 client
+const s3 = new AWS.S3();
+
+// Function to handle the file upload and save to S3
+function uploadFileToS3(file) {
+  const bucketName = 'siyabucket-s3';
+
+  const params = {
+    Bucket: bucketName,
+    Key: file.name,
+    Body: file
+  };
+
+  s3.putObject(params, (err, data) => {
+    if (err) {
+      console.log('Error uploading file:', err);
+    } else {
+      console.log('File uploaded successfully:', data);
+    }
   });
+}
+
+// Example event listener for the "Save" button
+const saveButton = document.getElementById('saveButton');
+saveButton.addEventListener('click', () => {
+  // Get the file input element
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+  
+  // Call the function to upload the file to S3
+  uploadFileToS3(file);
+});
